ad26b8589dd5b163cd7d566dce2c94dab47ec0d4
madventures05-ui <madventures05@gmail.com>
2026-06-06 14:36:31 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "🤍Helping people 🤍",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 4, 
       },
     ],
   },
@@ -88,8 +88,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
-      secondary: "#2F3136", 
+      primary: "#228B22", 
+      secondary: "	#7DF9FF", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
